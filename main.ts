e38d5275b7fe67ac7844faa34e38e9d3cbb65cf4
CaptainCredible <cpt.credible@gmail.com>
2023-02-05 11:55:56 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-let myVal = 0
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    led.toggleAll()
-myVal = Math.map(input.rotation(Rotation.Pitch), -180, 180, 0, 127)
+    p5midi.sendNote(0, 44)
+})
+basic.forever(function () {
+    p5midi.sendCC(0, Math.map(input.rotation(Rotation.Pitch), -90, 90, 0, 127))
+    p5midi.sendCC(1, Math.map(input.rotation(Rotation.Roll), -90, 90, 0, 127))
+    p5midi.sendCC(2, Math.map(input.lightLevel(), 0, 255, 0, 127))
+    led.toggle(2, 0)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
 })
